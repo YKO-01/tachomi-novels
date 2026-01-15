6423ea37841a845e42ad72d663da5065354f99f9
Ahmed Yakoubi <yk.yako01@gmail.com>
2026-01-15 17:17:02 +0100

    clean json files

--- lib/const.dart
+++ lib/const.dart
@@ -5,9 +5,10 @@ import 'package:tachomi_novel/ads/src/multi_ads_factory.dart';
 
 // https://drive.google.com/file/d//view?usp=sharing
 // https://drive.google.com/file/d/1g69qX0Utp6i3pLC2Q3pkcrh_3cUZLs_u/view?usp=sharing
+// https://drive.google.com/file/d/1dgSpUZiuOz712C7S1hZBUK4Lkgoc6Q6r/view?usp=sharing
 class Constants {
   static const jsonConfigUrl =
-      'https://drive.google.com/uc?export=download&id=1g69qX0Utp6i3pLC2Q3pkcrh_3cUZLs_u';
+      'https://drive.google.com/uc?export=download&id=1dgSpUZiuOz712C7S1hZBUK4Lkgoc6Q6r';
 
   static Color mainColor = const Color(0xff33FD24);
 }
